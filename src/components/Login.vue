66dbcf361711b2c3fbab1bc49c735485f0864694
erhard.karger <erhard.karger@vkb.de>
2019-01-25 09:55:16 +0100

    before change of main.ts

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,40 @@
 <template>
-<div id="Login">
-
-    <h1>
-    login
-    </h1>
-
-
-
-</div>
+<v-app>
+    <v-toolbar>
+        <v-toolbar-title>
+            Login
+        </v-toolbar-title>
+    </v-toolbar>
+    <div class="container text-xs-center">
+    <form @keyup.enter="my_login()">
+        <v-text-field v-model='email' label='E-mail'>
+        </v-text-field>
+        <v-text-field v-model='password' label='Password' type="password">
+        </v-text-field>
+    </form>
+    <v-btn @click="my_login()">
+        Login
+    </v-btn>
+    </div>
+</v-app>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-
-@Component
-export default class Login extends Vue {
-}
+import Vue from 'vue';
+export default Vue.extend(  {
+    data() {
+        return {
+        email: '',
+        password: '',
+        };
+    },
+    methods: {
+       my_login() {
+        this.$emit('logged in', {})
+        console.log('say Hello');
+       },
+    },
+});
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
--- src/components/__tests__/login.spec.ts
+++ src/components/__tests__/login.spec.ts
@@ -1,17 +1,51 @@
-import { shallowMount } from '@vue/test-utils';
+import Vue from 'vue';
+import { shallowMount,mount } from '@vue/test-utils';
 import Login from '../Login.vue';
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
 
 describe('login.vue', () => { // #A
     test('sanity test', () => {
         expect(true).toBe(true);
-            });
-        });
-
+    });
+});
 
 describe('Login.vue', () => {
-      test('renders the login component', () => {
-            const wrapper = shallowMount(Login);
-            expect(wrapper.text()).toContain('Login');
+    it('renders the login component', () => {
+        let wrapper = shallowMount(Login);
+        expect(wrapper.text()).toContain('Login');
           });
+
+    it('should have a input field', () => {
+        let wrapper = mount(Login);
+        expect(wrapper.findAll('input').length).toBe(2);
+    });
+
+    it('calls login when button is clicked', () => {
+        let my_login = jest.fn();
+        let wrapper = mount(Login,{
+            methods: {
+                my_login
+            }
+        });
+        wrapper.find('button').trigger('click'); 
+        expect(my_login).toHaveBeenCalled();
+    });
+
+
+
+    it('should emit an event, when clicked on login', () => {
+        let wrapper = mount(Login);
+        wrapper.find('button').trigger('click');
+        expect(wrapper.emitted('logged in')).toHaveLength(1);
+     });
+
+
+
+
+
+
+
+
 });
 
