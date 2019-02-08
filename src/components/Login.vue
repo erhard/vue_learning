3672e10b741145878f2711d55ff8f7f2e556030a
erhard.karger <erhard.karger@vkb.de>
2019-02-08 15:27:14 +0100

    on key pressed and Binding works

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,26 +1,36 @@
 <template>
-
-
-
 <div :style="style">
     <!-- Render textfield component -->
-    <div >
-        <div class="mdc-text-field    
-            username"   ref="userName" >
-            <input type="text" class="mdc-text-field__input" id="username-input" name="username">
-            <label class="mdc-floating-label" for="username-input">Username</label>
-            <div class="mdc-line-ripple"></div>
+    <div class="mdc-card mdc-typography ">
+    aaaaaaaaa
+        {{user}}
+        {{password}}
+        bbbbbb
+        <h2 class="mdc-typography--headline2">Login</h2>
+        <div class="mdc-text-field  username"   ref="userName" >
+                <input type="text" class="mdc-text-field__input" 
+                id="username-input" name="user"
+                v-model="user" v-on:keyup="checkForm">
+                <label class="mdc-floating-label" for="username-input">Username</label>
+                <div class="mdc-line-ripple"></div>
+            </div>
+            <div class="mdc-text-field password" ref="pwd">
+                <input type="password" class="mdc-text-field__input" 
+                id="password-input" name="password"
+                v-model="password" v-on:keyup="checkForm"
+                >
+                <label class="mdc-floating-label" for="password-input">Password</label>
+                <div class="mdc-line-ripple"></div>
+            </div>
+        <div class="mdc-card__actions">
+            <button class="mdc-button mdc-button--raised mdc-card__action--button" ref="sendButton"
+                @click="my_login">
+                <span class="mdc-button__label">login</span>
+            </button>
+
         </div>
-        <div class="mdc-text-field password" ref="pwd">
-            <input type="password" class="mdc-text-field__input" id="password-input" name="password">
-            <label class="mdc-floating-label" for="password-input">Password</label>
-            <div class="mdc-line-ripple"></div>
-                </div>
-                <button class="mdc-button mdc-button--raised" ref="sendButton">
-                    <span class="mdc-button__label">login</span>
-        </button>
     </div>
-        </div>
+</div>
 </template>
 
 <script lang="ts">
@@ -30,16 +40,27 @@ import {MDCRipple} from '@material/ripple';
 export default Vue.extend(  {
     data() {
         return {
-            email: '',
+            user: '',
             password: '',
+            valid: false
         };
     },
     methods: {
         my_login() {
             this.$emit('clicked', {"data": "datafrom innerMost"});
         },
-    },
+        
+       checkForm(event) {
+          console.log("Im checkForm");
+          console.log(this.user);
+          console.log(this.password);
+          console.log(event.key);
+        },
+        
 
+
+
+    },
     mounted() { 
         new MDCTextField(this.$refs.userName);
         new MDCTextField(this.$refs.pwd);
@@ -66,7 +87,8 @@ export default Vue.extend(  {
 @import '../assets/mdc.floating-label.css';
 @import '../assets/mdc.line-ripple.css';
 @import '../assets/mdc.button.css';
-
+@import '../assets/mdc.card.css';
+@import '../assets/mdc.typography.css';
 .mdc-text-field--focused:not(.mdc-text-field--disabled) .mdc-floating-label {
   color: var(--mdc-floating-label-focused, black);
 }
